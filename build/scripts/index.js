f9b9951b874e6ccf9c4fedac72d27f9626a9c11e
zZ <z@zZ.local>
2016-06-12 00:11:06 +0800

    fix nav

--- build/scripts/index.js
+++ build/scripts/index.js
@@ -2,9 +2,11 @@
 $(function () {
     var navBtn = $('.js_nav_btn');
     navBtn.on('click', function () {
-        var thisRight = $('.index').css('right') == '0px' ? '10.6rem' : '0rem';
+        var thisRight   = $('.index').css('right') == '0px' ? '10.6rem' : '0rem';
+        var thisDisplay = $('.index').css('right') == '0px' ? 'block' : 'none';
         $('.index').animate({
-            right: thisRight
+            right: thisRight,
+            display: thisDisplay
         });
     });
 });
