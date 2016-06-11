d2b9e73cc0ef32547bb96f4287e063f4b979e74d
zZ <z@zZ.local>
2016-06-11 23:55:38 +0800

    add index

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,2 +1,10 @@
 'use strict';
-console.log('index');
+$(function () {
+    var navBtn = $('.js_nav_btn');
+    navBtn.on('click', function () {
+        var thisRight = $('.index').css('right') == '0px' ? '10.6rem' : '0rem';
+        $('.index').animate({
+            right: thisRight
+        });
+    });
+});
