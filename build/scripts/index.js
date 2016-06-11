c6b72ebcd98f19bce2e3be7175dc276864dd3ce8
zZ <z@zZ.local>
2016-06-12 00:13:16 +0800

    fix nav

--- build/scripts/index.js
+++ build/scripts/index.js
@@ -4,9 +4,11 @@ $(function () {
     navBtn.on('click', function () {
         var thisRight   = $('.index').css('right') == '0px' ? '10.6rem' : '0rem';
         var thisDisplay = $('.index').css('right') == '0px' ? 'block' : 'none';
-        $('.index').animate({
-            right: thisRight,
+        $('.nav').css({
             display: thisDisplay
+        });
+        $('.index').animate({
+            right: thisRight
         });
     });
 });
